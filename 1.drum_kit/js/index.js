e54155a9b35f6f2724f659cc67a4a9d9a573371f
Naman <namanmittal0007@gmail.com>
2021-06-23 07:11:11 +0530

    adding comments

--- 1.drum_kit/js/index.js
+++ 1.drum_kit/js/index.js
@@ -1,6 +1,7 @@
+//cb func to play sound corresponding to the pressed key
 function playsound(e){
-    let audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    let key = this.document.querySelector(`div.keys[data-keys="${e.keyCode}"]`);
+    let audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);   //Extract audio for key
+    let key = this.document.querySelector(`div.keys[data-keys="${e.keyCode}"]`); //selecting the element corresponding key
     if(!audio) return;
     else{
         audio.currentTime=0;
@@ -8,11 +9,17 @@ function playsound(e){
         key.classList.add('playing');
     }
 }
+
+//cb func to end the transition from any key element
 function endtransition(e){
-    if(e.propertyName != "transform") return;
-    this.classList.remove('playing');
+    if(e.propertyName != "transform") return;       //check if the transform took place for this element 
+    this.classList.remove('playing');               //remove class playing from the element
 }
+
+
+//selecting all the elements with class .keys
 let keys = document.querySelectorAll('.keys');
+//looping over keys to check if transion end and call endtransition func
 keys.forEach( key => key.addEventListener("transitionend",endtransition))
+//checking for window if any keydown(key is being pressed) and calling playsound
 window.addEventListener("keydown",playsound);
-window.addEventListener("click",playsound);
